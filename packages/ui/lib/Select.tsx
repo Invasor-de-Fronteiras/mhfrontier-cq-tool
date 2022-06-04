dcff0cc24f06abf6a33459f9ebe22ab4b8968aed
Rainã Pepe <raina.pepe@outlook.com>
2022-06-04 20:05:51 -0300

    fix: react-select

--- packages/ui/lib/Select.tsx
+++ packages/ui/lib/Select.tsx
@@ -1,24 +1,25 @@
 import type { ComponentProps } from "react";
 import { useState } from "react";
-import ReactSelect from "react-select";
+import ReactSelect, { StylesConfig } from "react-select";
 
 interface SelectProps extends ComponentProps<ReactSelect> {
   index: number;
 }
 
 export function Select(props: SelectProps) {
-  // https://github.com/JedWatson/react-select/issues/1537#issuecomment-868383410
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
-
   return (
-    <>
-    {menuIsOpen}
     <ReactSelect
       {...props}
-      onMenuOpen={() => setMenuIsOpen(!menuIsOpen)}
-      onMenuClose={() => setMenuIsOpen(!menuIsOpen)}
-      styles={{ container: (styles) => ({ ...styles, zIndex: menuIsOpen ? 999 : 0  }) }}
+      // https://github.com/JedWatson/react-select/issues/1537#issuecomment-868383410
+      // @ts-ignore
+      menuPortalTarget={document.body}
+      theme={(theme) => ({
+          ...theme,
+         colors: {
+           ...theme.colors,
+           primary: 'rgb(4 120 87 / var(--tw-text-opacity))',
+         },
+      })}
     />
-    </>
   );
 }
