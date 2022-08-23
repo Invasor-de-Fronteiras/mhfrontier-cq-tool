5f15766112e086c04966f28121cbccfb618996d0
Rainã Pepe <raina.pepe@outlook.com>
2022-08-23 20:31:12 -0300

    feat(questlist): questlist tab refactor

--- packages/ui/lib/tabs/questlist/QuestlistsTab.tsx
+++ packages/ui/lib/tabs/questlist/QuestlistsTab.tsx
@@ -1,13 +1,56 @@
 import classNames from "classnames";
 import { useMemo, useState } from "react";
 import { useWatch } from "react-hook-form";
-import { Button } from "../../components/Button";
 import { GroupCard } from "../../components/CardGroup";
+import { Select } from "../../components/Select";
 import { useQuestlistEditor } from "../../context/QuestlistEditorContext";
-import { getQuestCategory, getQuestMark } from "../../utils";
+import { getQuestCategory, getQuestMark, QuestlistFile } from "../../utils";
 
 interface QuestlistQuestsProps {
-    questlistIndex: number;
+    questlistIndex: number | null;
+}
+
+interface QuestInfoRow {
+    filename: string;
+    questId: number;
+    title: string;
+    mainObjective: string;
+    questCategory: string;
+    mark: string;
+    maxPlayers: number;
+}
+
+interface QuestlistRowProps {
+    quest: QuestInfoRow;
+    showQuestlist?: boolean;
+}
+
+export const QuestlistRow = ({ quest, showQuestlist }: QuestlistRowProps) =>
+    <tr
+        // key={`${quest.filename}-${quest.questId}`}
+        className={classNames("hover:bg-emerald-300 cursor-pointer")}
+    // onClick={() => setSelectedIndex(i === selectedIndex ? null : i)}
+    >
+        {showQuestlist && <td className="px-6 py-4" scope="row">{quest.filename}</td>}
+        <td className="px-6 py-4">{quest.title}</td>
+        <td className="px-6 py-4">{quest.mainObjective}</td>
+        <td className="px-6 py-4">{quest.questCategory}</td>
+        <td className="px-6 py-4">{quest.mark}</td>
+        <td className="px-6 py-4">{quest.maxPlayers}</td>
+    </tr>;
+
+const mapQuestlist = (questlist?: QuestlistFile): QuestInfoRow[] => {
+    if (!questlist) return [];
+
+    return questlist.quests.map<QuestInfoRow>(quest => ({
+        filename: questlist.filename,
+        questId: quest.quest_type_flags.main_quest_prop.quest_id,
+        title: quest.strings.title,
+        mainObjective: quest.strings.main_objective,
+        questCategory: getQuestCategory(quest.header.quest_category),
+        mark: getQuestMark(quest.header.mark),
+        maxPlayers: quest.header.max_players,
+    }));
 }
 
 export function QuestlistQuests({ questlistIndex }: QuestlistQuestsProps) {
@@ -17,83 +60,81 @@ export function QuestlistQuests({ questlistIndex }: QuestlistQuestsProps) {
         name: 'questlists',
     });
 
-    const questlist = useMemo(() => questlists[questlistIndex] || [], [questlists, questlistIndex]);
-    
+    const questlist = useMemo<QuestInfoRow[]>(() => {
+        if (questlistIndex === null)
+            return questlists.reduce<QuestInfoRow[]>((acc, cur) => {
+                acc.push(...mapQuestlist(cur));
+                return acc;
+            }, []);
+
+        return mapQuestlist(questlists[questlistIndex]);
+    }, [questlists, questlistIndex]);
+
     return (
-    <table
-        aria-label="Quests"
-        className="shadow-sm  w-full text-sm text-left"
-    >
-        <caption>click on which quest you want to edit</caption>
-        <thead className="text-xs uppercase">
-            <tr className="dark:text-white">
-                <th role="columnheader" scope="col" className="px-6 py-4">
-                    Title
-                </th>
-                <th role="columnheader" scope="col" className="px-6 py-4">
-                    Description
-                </th>
-                <th role="columnheader" scope="col" className="px-6 py-4">
-                    Main Objective
-                </th>
-                <th role="columnheader" scope="col" className="px-6 py-4">
-                    Category
-                </th>
-                <th role="columnheader" scope="col" className="px-6 py-4">
-                    mark
-                </th>
-                <th role="columnheader" scope="col" className="px-6 py-4">
-                    Max players
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            {questlist.quests.map((quest) => {
-                return (
-                    <tr
-                        key={quest.quest_type_flags.main_quest_prop.quest_id}
-                        className={classNames("hover:bg-emerald-300 cursor-pointer", {
-                            // "bg-emerald-300": i === selectedIndex,
-                        })}
-                        // onClick={() => setSelectedIndex(i === selectedIndex ? null : i)}
-                    >
-                    <th className="px-6 py-4" scope="row">
-                        {quest.strings.title ?? "--"}
+        <table
+            aria-label="Quests"
+            className="shadow-sm  w-full text-sm text-left"
+        >
+            <caption>click on which quest you want to edit</caption>
+            <thead className="text-xs uppercase">
+                <tr className="dark:text-white">
+                    {questlistIndex === null && <th role="col" scope="col" className="px-6 py-4">
+                        Questlist
+                    </th>}
+                    <th role="columnheader" scope="col" className="px-6 py-4">
+                        Title
                     </th>
-                    <td className="px-6 py-4">
-                        {quest.strings.description ?? "--"}
-                    </td>
-                    <td className="px-6 py-4">{quest.strings.main_objective ?? "--"}</td>
-                    <td className="px-6 py-4">{getQuestCategory(quest.header.quest_category)}</td>
-                    <td className="px-6 py-4">{getQuestMark(quest.header.mark)}</td>
-                    <td className="px-6 py-4">{quest.header.max_players}</td>
-                    </tr>
-                );
-            })}
-        </tbody>
-    </table>
+                    <th role="columnheader" scope="col" className="px-6 py-4">
+                        Main Objective
+                    </th>
+                    <th role="columnheader" scope="col" className="px-6 py-4">
+                        Category
+                    </th>
+                    <th role="columnheader" scope="col" className="px-6 py-4">
+                        mark
+                    </th>
+                    <th role="columnheader" scope="col" className="px-6 py-4">
+                        Max players
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {questlist.map((quest, i) => {
+                    return <QuestlistRow
+                        key={`${i}${questlistIndex}-${quest.filename}-${quest.questId}`}
+                        quest={quest}
+                        showQuestlist={questlistIndex === null}
+                    />
+                })}
+            </tbody>
+        </table>
     );
 }
 
 export function QuestlistTab() {
-  const { form } = useQuestlistEditor();
-  const [selected, setSelected] = useState<null | number>(null);
-  const questlists = useWatch({ control: form.control,  name: 'questlists' });
+    const { form } = useQuestlistEditor();
+    const [selected, setSelected] = useState<null | number>(null);
+    const questlists = useWatch({ control: form.control, name: 'questlists' });
 
-  console.log('questlists: ', questlists);
-  if (!questlists || !questlists.map) return <div>Empthy</div>;
+    if (!questlists || !questlists.map) return <div>Empthy</div>;
 
-  return (
-    <div className="flex flex-row flex-wrap gap-2">
-        <GroupCard title="Main">
-          {questlists.map((v, i) => (<div key={v.filename} className="right-0 mt-3 mr-4">
-            {v.filename}
-            <Button onClick={() => setSelected(i)} />
-          </div>))}
-        </GroupCard>
-        <GroupCard title="Main">
-            {selected !== null && <QuestlistQuests questlistIndex={selected} />}
-        </GroupCard>
-    </div>
-  );
+    return (
+        <div className="flex flex-row flex-wrap gap-2">
+            <GroupCard title="Questlist">
+                <Select
+                    options={questlists.map((v, i) => ({ label: v.filename, value: i }))}
+                    label="Questlist"
+                    isClearable
+                    onChange={item => {
+                        if (item === null) return setSelected(null);
+                    
+                        setSelected(item.value);
+                    }}
+                />
+            </GroupCard>
+            <GroupCard title="Quests">
+                <QuestlistQuests questlistIndex={selected} />
+            </GroupCard>
+        </div>
+    );
 }
